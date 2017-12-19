cbe2325857cfaac88551bfc4011e044322ed324d
Niels Verheijen <kahlan147@gmail.com>
2017-12-19 10:30:47 +0100

    W15 extra class to show 1

---
+++ src/mylogic/sockets/ReceivingClientSocketInstant.java
@@ -0,0 +1,66 @@
+package mylogic.sockets;
+
+import calculate.Edge;
+
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Niels Verheijen on 19/12/2017.
+ */
+public class ReceivingClientSocketInstant {
+
+    private ReceivingClientSocketInstant()
+    {}
+
+    public static List<Edge> getEdges(int level) throws IOException
+    {
+        Socket socket = new Socket("127.0.0.1", 8100);
+
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+
+        objectOutputStream.writeInt(Integer.valueOf(level));
+        objectOutputStream.flush();
+
+        List<Edge> edges = new ArrayList<>();
+
+        ObjectInputStream objectInputStream = new ObjectInputStream(socket.getInputStream());
+
+        Object readobject = null;
+
+        try
+        {
+            readobject = objectInputStream.readObject();
+
+            while (readobject != null)
+            {
+                if (readobject instanceof Edge)
+                {
+                    edges.add((Edge) readobject);
+                }
+                else
+                {
+                    System.out.println("Something went wrong with receiving edges...");
+                }
+
+                readobject = objectInputStream.readObject();
+            }
+        }
+        catch (ClassNotFoundException e)
+        {
+            e.printStackTrace();
+        }
+        catch (EOFException ignored)
+        { }
+
+        objectOutputStream.close();
+        objectInputStream.close();
+
+        return edges;
+    }
+}
